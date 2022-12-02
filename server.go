dec0b340ea8d45190c6a7a2748623eca87d8fb73
ilies <iliesassaddev@gmail.com>
2022-12-02 22:42:52 +0100

    update

--- server.go
+++ server.go
@@ -1,27 +1,16 @@
 package main
 
 import (
-	"sync"
-
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
-	srv1 := gin.New()
-	srv2 := gin.New()
+	srv := gin.New()
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go runSrv(srv1, ":3000", "srv1")
-	go runSrv(srv2, ":5000", "srv2")
-	wg.Wait()
-}
-
-func runSrv(srv *gin.Engine, port string, message string) {
 	srv.GET("/", func(c *gin.Context) {
-		c.JSON(200, message)
+		c.JSON(200, "srv")
 	})
 
-	srv.Run(port)
+	srv.Run(":8080")
 }
